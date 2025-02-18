b8aafed108c78898934da2c893c6eb0dcc6a1ba4
Andypandita04 <andreafigueroa04@gmail.com>
2025-02-18 15:58:04 -0600

    Clase HealthController

---
+++ src/main/HealthController.kt
@@ -0,0 +1,17 @@
+// 
+package com.unam.fciencias.bern_demo_api.modules.config.framework.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/health")
+class HealthController {
+
+    @GetMapping
+    fun retrieveHealth(): ResponseEntity<String> {
+        return ResponseEntity.ok("HOLA MUNDO :DDD")
+    }
+}

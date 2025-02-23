03e29e3bd8c2dc13116dfcba6fb20d5534131e03
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2025-02-23 16:35:46 -0600

    Inicio app

---
+++ src/main/kotlin/com/unam/fciencias/urbanincidents/UrbanIncidentsApplication.kt
@@ -0,0 +1,11 @@
+package com.unam.fciencias.urbanincidents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrbanIncidentsApplication
+
+fun main(args: Array<String>) {
+    runApplication<UrbanIncidentsApplication>(*args)
+}

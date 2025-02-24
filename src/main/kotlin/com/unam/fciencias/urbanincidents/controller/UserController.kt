9b572d73bc918bd7ae9bb4a2aa8dbe5ef1d4a118
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2025-02-23 19:33:30 -0600

    1er endpoint terminado

--- src/main/kotlin/com/unam/fciencias/urbanincidents/controller/UserController.kt
+++ src/main/kotlin/com/unam/fciencias/urbanincidents/controller/UserController.kt
@@ -8,16 +8,25 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/v1/users")
 class UserController {
 
-
+    /**
+     * Endpoint for creating a new user.
+     * This method handles HTTP POST requests to create a new user in the system.
+     * @param user The user object received in the request body containing the user's details
+     *             such as email, password, and token.
+     * @return ResponseEntity containing the created User object in the response body
+     *         with HTTP status 200 (OK).
+     */
     @PostMapping
     fun createUser(@RequestBody user: User): ResponseEntity<User> {
-        TODO("Por implementar")
+        val myUser = User(user.email, user.password, user.token)
+        return ResponseEntity.ok(myUser)
     }
 
     // Endpoint for user login
     @PostMapping("/login")
     fun loginUser(@RequestBody loginRequest: Map<String, String>): ResponseEntity<User> {
         TODO("Por implementar")
+
     }
 
     // Endpoint for user logout
